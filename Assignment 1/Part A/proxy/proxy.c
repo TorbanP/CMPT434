e3194636fb58b248c5341ebb09eec9042267315d
Torban Peterson <torban@torban.ca>
2016-01-20 18:29:16 -0600

    Add netbeans project files, compile flags

---
+++ Assignment 1/Part A/proxy/proxy.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   proxy.c
+ * Author: tpeterson
+ *
+ * Created on January 20, 2016, 6:21 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ Assignment 1/Part A/server/server.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   server.c
+ * Author: tpeterson
+ *
+ * Created on January 20, 2016, 6:22 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
