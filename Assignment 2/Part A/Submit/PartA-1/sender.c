ae4b21f49fd8aff97954641d09a82abd3bb470d9
Torban Peterson <torban@torban.ca>
2016-03-02 01:05:50 -0600

    part a1 done

--- Assignment 2/Part A/Submit/PartA-1/sender.c
+++ Assignment 2/Part A/Submit/PartA-1/sender.c
@@ -25,14 +25,11 @@
 #define SEQSIZE 6
 #define INITSEQID 0
 
+/* contains a list of messages to send. originally had more items inside hence the struct */
 typedef struct {
     char data[SEQSIZE + DATASIZE];
-    
+
 } frame;
-
-//int GL_LAR; /* Last ACK received */
-int GL_LFS; /* Last Frame Sent */
-
 
 /* 
  * main
@@ -43,15 +40,13 @@ int GL_LFS; /* Last Frame Sent */
  * timeout value (in seconds)
  * the number of lines to send
  */
-int main(int argc, char *argv[])
-{
-    int *LAR = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+int main(int argc, char *argv[]) {
+    int *LAR = mmap(NULL, sizeof (int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    int GL_LFS;
     int timeout = atoi(argv[4]);
     struct timeval start, stop;
-    
-    
-    *LAR = INITSEQID;
-    GL_LFS = INITSEQID;
+    *LAR = INITSEQID; /* last ack received */
+    GL_LFS = INITSEQID; /* last frame sent  */
     int sockfd;
     struct sockaddr_storage their_addr;
     socklen_t addr_len;
@@ -65,28 +60,26 @@ int main(int argc, char *argv[])
     }
 
     /* the frame data storage */
-    
+
     /* find num lines in the file */
     FILE *linecount;
     int temp = 0;
-    int lines = 1;
+    int lines = 0;
     linecount = fopen("data", "r");
     if (linecount == NULL)
         exit(EXIT_FAILURE);
-    
-    while ((temp = fgetc(linecount)) != EOF)
-    {
-      if (temp == '\n')
-    lines++;
+
+    while ((temp = fgetc(linecount)) != EOF) {
+        if (temp == '\n')
+            lines++;
     }
     fclose(linecount);
 
     frame frame_array[lines];
-    
+
     /* get messages from file to save time , hardcoded but meh, store in frame_array*/
 
     size_t buffsize = DATASIZE;
-    
     FILE *stream;
     stream = fopen("data", "r");
     if (stream == NULL)
@@ -94,19 +87,17 @@ int main(int argc, char *argv[])
     int i = 0;
     int seq_id = INITSEQID;
     char* temp_msg[DATASIZE];
-    while(fgets(temp_msg, buffsize, stream)){
+    while (fgets(temp_msg, buffsize, stream)) {
         seq_id = i + INITSEQID;
         snprintf(frame_array[i].data, 10, "%d", seq_id);
         strcat(frame_array[i].data, " ");
         strcat(frame_array[i].data, temp_msg);
-        
-       // frame_array[i].seq_id = i + INITSEQID;
-        printf("%s",frame_array[i].data);
+        printf("%s", frame_array[i].data);
         i++;
     }
-     fclose(stream);
-     
-    /* UDP stuff */
+    fclose(stream);
+
+    /* UDP setup stuff */
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
@@ -118,12 +109,11 @@ int main(int argc, char *argv[])
     }
 
     /* loop through all the results and make a socket */
-    for(p = servinfo; p != NULL; p = p->ai_next) {
+    for (p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("talker: socket");
             continue;
         }
-
         break;
     }
 
@@ -135,12 +125,11 @@ int main(int argc, char *argv[])
     freeaddrinfo(servinfo);
 
     pid_t pid = fork();
-
     if (pid == 0) {
         /* child - listens for ACK, increments GL_LAR*/
         char buf[DATASIZE];
         addr_len = sizeof their_addr;
-        
+
         while (1) {
             if ((numbytes = recvfrom(sockfd, buf, DATASIZE - 1, 0, (struct sockaddr *) &their_addr, &addr_len)) == -1) {
                 perror("recvfrom");
@@ -151,19 +140,23 @@ int main(int argc, char *argv[])
                 *LAR = *LAR + 1;
                 printf(" = ok, LAR = %d\n", *LAR);
             }
-
         }
     } else {
-        
-        
+        /* parent - sends frames */
         while (1) {
 
             /* check if i can send data to receiver */
             int LARtemp = *LAR;
             int j = 0;
             while (GL_LFS - LARtemp + 1 < strtol(argv[3], NULL, 10)) {
-                
-                if (j == 0){
+                /* check that there are still messages to run*/
+                if (j + LARtemp >= lines) {
+                    break;
+                } else if (LARtemp > lines) {
+                    return 0;
+                }
+                /* oldest packet for timeout check */
+                if (j == 0) {
                     gettimeofday(&start, NULL);
                 }
 
@@ -173,16 +166,13 @@ int main(int argc, char *argv[])
                 }
                 GL_LFS = LARtemp + j;
                 j++;
-                printf("sender: sent %d bytes to %s = %s\n", numbytes, argv[1], frame_array[GL_LFS].data);
-
+                printf("sender: sent %d bytes to %s = %s", numbytes, argv[1], frame_array[GL_LFS].data);
             }
-            
-            /* check if oldest sent has timed out */
+            /* check if oldest sent has timed out, reset LFS if so to trigger retry*/
             gettimeofday(&stop, NULL);
-            if ((stop.tv_sec - start.tv_sec) > timeout){
+            if ((stop.tv_sec - start.tv_sec) > timeout) {
                 GL_LFS = *LAR;
-            } 
-
+            }
         }
         close(sockfd);
 
---
+++ Assignment 2/Part A/part2/sender/build/Debug/GNU-Linux-x86/sender.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux-x86/sender.o: sender.c
